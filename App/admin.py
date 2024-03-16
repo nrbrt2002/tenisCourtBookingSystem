b73d452fb4136601f7e73e0de16b67167d7bcbe9
Norbert <nrbrt2002@gmail.com>
2024-03-16 20:44:22 +0200

    First commit before trying to develop the external model for images

---
+++ App/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+from App.models import Court
+
+# Register your models here.
+
+admin.site.register(Court)
---
+++ App/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+# Create your models here.
+
+COURT_TYPES = (
+    ('clay', 'Clay'),
+    ('grass', 'Grass'),
+    ('hardcourt', 'Hardcourt'),
+)
+COURT_LOCATION = (
+    ('indoors', 'Indoors'),
+    ('outside', 'Out-side'),
+)
+
+class Court(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    court_type = models.CharField(max_length=20, choices=COURT_TYPES)
+    court_location = models.CharField(max_length=20, choices=COURT_LOCATION)
+    has_lighting = models.BooleanField(default=False)
+    is_available = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_at']
+    
+    def __str__(self):
+        return self.name
